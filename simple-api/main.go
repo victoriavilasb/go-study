55914ecfc957c00a8b445580b901870c6939a6a2
Victoria Olivia <victoria@iMac-de-Victoria.local>
2020-09-01 09:21:19 -0300

    simple api

---
+++ simple-api/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+	"log"
+)
+
+type Room struct {
+	Name string `json:"name"`
+	Guid string `json:"guid"`
+	Capacity int `json:"capacity"`
+	HostUser string `json:"host_user"`
+	Participants []string `json:"participants"`
+}
+
+var Rooms []Room
+
+func handleRequests() {
+	http.HandleFunc("/rooms", listRooms)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	Rooms = []Room{
+		Room{Name: "Games", Guid: "1", Capacity: 5, HostUser: "victoriaovilas", Participants: []string{
+			"victoriaovilas", "pefreis",
+		}},
+		Room{Name: "Book Club", Guid: "2", Capacity: 10, HostUser: "helencunha", Participants: []string{
+			"helencunha", "flaviog", "victoriaovilas", "pefreis",
+		}},
+	}
+	handleRequests()
+}
+
+func listRooms(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: listRooms")
+	json.NewEncoder(w).Encode(Rooms)
+}
+
