38a752be1add65142ebe145b83197d8cf900dc63
Victoria Olivia <victoria@iMac-de-Victoria.local>
2020-09-21 23:48:47 -0300

    turn findian not case sensitive

--- coursera-especialization/getting-started/week-2/findian/findian.go
+++ coursera-especialization/getting-started/week-2/findian/findian.go
@@ -9,6 +9,8 @@ func main()  {
 	var str string
 	fmt.Scanf("%s", &str)
 
+	str = strings.ToLower(str)
+
 	if strings.HasPrefix(str, "i") && strings.HasSuffix(str, "n") && strings.ContainsAny(str, "a") {
 		fmt.Println("Found!")
 	} else {
